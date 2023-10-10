00f545356b8e9e9608d31503021bab013d8338c7
DiegoMoyan0 <diegomoyano02@gmail.com>
2023-10-10 16:30:12 -0300

    add media queries

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
   var titleAbout = document.getElementById("title-about");
   var lineaVerde = document.querySelector(".linea-verde");
 
-  // Función para comprobar si el elemento está en la vista del usuario
   function isInViewport(element) {
     var rect = element.getBoundingClientRect();
     return (
@@ -13,25 +12,19 @@ document.addEventListener("DOMContentLoaded", function() {
     );
   }
 
-  // Función para manejar el evento de scroll
   function handleScroll() {
     if (isInViewport(titleAbout)) {
       titleAbout.classList.add("visible");
 
-      // Agregar la clase "visible" y "loading" a la barra
       lineaVerde.classList.add("visible");
       lineaVerde.classList.add("loading");
 
-      // Eliminar la clase "loading" cuando la barra esté completa
       setTimeout(function() {
         lineaVerde.classList.remove("loading");
-      }, 2000); // 2 segundos para que coincida con la duración de la animación de la barra
+      }, 2000); 
 
-      // Eliminar el listener después de la animación para que no se repita
       window.removeEventListener("scroll", handleScroll);
     }
   }
-
-  // Agregar un listener de scroll
   window.addEventListener("scroll", handleScroll);
 });
