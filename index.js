c6c3ee0a20ea72718b3d2a388b2f43f2577f9b47
DiegoMoyan0 <diegomoyano02@gmail.com>
2023-10-26 18:14:49 -0300

    changes in animations titles

--- index.js
+++ index.js
@@ -1,30 +1,58 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var titleAbout = document.getElementById("title-about");
-  var lineaVerde = document.querySelector(".linea-verde");
+// window.addEventListener('scroll', function() {
+//   var element = document.getElementById('title-about');
+//   var position = element.getBoundingClientRect();
 
-  function isInViewport(element) {
-    var rect = element.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
+//   // Aparece el texto cuando el elemento está a la vista
+//   if (position.top < window.innerHeight) {
+//       element.style.visibility = 'visible';
+//       element.style.opacity = '1';
+//   }
+// });
+
+window.addEventListener('scroll', function() {
+  var element = document.getElementById('title-about');
+  var position = element.getBoundingClientRect();
+
+  if (position.top < window.innerHeight) {
+      element.classList.remove('hidden');
+      element.classList.add('slide-in');
   }
+});
 
-  function handleScroll() {
-    if (isInViewport(titleAbout)) {
-      titleAbout.classList.add("visible");
+window.addEventListener('scroll', function() {
+  var element = document.getElementById('title-services');
+  var position = element.getBoundingClientRect();
 
-      lineaVerde.classList.add("visible");
-      lineaVerde.classList.add("loading");
-
-      setTimeout(function() {
-        lineaVerde.classList.remove("loading");
-      }, 2000); 
-
-      window.removeEventListener("scroll", handleScroll);
-    }
+  if (position.top < window.innerHeight) {
+      element.classList.remove('hidden');
+      element.classList.add('slide-in');
   }
-  window.addEventListener("scroll", handleScroll);
+});
+
+window.addEventListener('scroll', function() {
+  var element = document.getElementById('title-dife');
+  var position = element.getBoundingClientRect();
+
+  if (position.top < window.innerHeight) {
+      element.classList.remove('hidden');
+      element.classList.add('slide-in');
+  }
+});
+window.addEventListener('scroll', function() {
+  var element = document.getElementById('title-contact');
+  var position = element.getBoundingClientRect();
+
+  if (position.top < window.innerHeight) {
+      element.classList.remove('hidden');
+      element.classList.add('slide-in');
+  }
+});
+window.addEventListener('scroll', function() {
+  var element = document.getElementById('p-contact');
+  var position = element.getBoundingClientRect();
+
+  if (position.top < window.innerHeight) {
+      element.classList.remove('hidden');
+      element.classList.add('slide-in');
+  }
 });
